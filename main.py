5925507b1e611dcd641c4740ca6de953a33ffbc1
Shangirna <74745662+Shangirna@users.noreply.github.com>
2022-10-22 23:21:02 -0400

    Minor changes

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ from sklearn import tree, metrics, decomposition, svm
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.feature_extraction.text import CountVectorizer
 
-pathToFile = "/Users/kawtherbouzeghaya/Downloads/goemotions.json"
+pathToFile = "goemotions.json"
 redditData = pd.read_json(pathToFile)
 file = open(pathToFile)
 loadedData = json.load(file)
 redditPosts = np.array(loadedData)
 sentimentClasses = ['positive', 'neutral', 'negative', 'ambiguous']
 # create a file that contains all the information.
-performance = open("/Users/kawtherbouzeghaya/Desktop/COMP472_MiniProject1/performance.txt",'w')
+performance = open("performance.txt",'w')
 myRepeatedEmotionsList = redditData[1]  # We first need to get the list of emotions from the data set
 # Then we will need to remove the repeated values and convert the list back.
 myUnrepeatedEmotionsClassesList = dict.fromkeys(myRepeatedEmotionsList)
@@ -168,8 +168,8 @@ parametersGrid = {'alpha': [0.5, 0, 2, 1, 0.75]}
 gridb = GridSearchCV(mnb, param_grid=parametersGrid, cv=5)
 gridb.fit(X_train, Z_train)
 sentimentTopMNBprediction = gridb.predict(X_test)
-print("Emotion MNB best score is ", gridb.best_score_)
-print("Emotion MNB best hyper parameter is ", gridb.best_params_)
+print("Sentiment MNB best score is ", gridb.best_score_)
+print("Sentiment MNB best hyper parameter is ", gridb.best_params_)
 #---------------------------------------------------------------
 
 #---------------------------2.3.5 TOP DT------------------------
@@ -189,7 +189,7 @@ print("Emotion DT best score is ", grid.best_score_)
 print("Emotion DT best hyper parameters are ", grid.best_params_)
 
 # Sentiments:
-print("------Sentimenet Decision Tree analysis (hyper parameters)--------")
+print("------Sentiments Decision Tree analysis (hyper parameters)--------")
 grid2 = GridSearchCV(sentimentDecisionTree, param_grid=parametersGrid,cv=5)
 grid2.fit(X_train,Z_train)
 sentimentTopDecisionTreePrediction = grid2.predict(X_test)
@@ -400,16 +400,16 @@ performance.write("-------------------------------------------------------------
 # Base
 print('----------------------------------------------------')
 print('Multinomial Naive-Bayes for sentiments:')
-mnb.fit(X_train_stopWords,Y_train)
+mnb.fit(X_train_stopWords,Z_train)
 y_pred_stopWords_MNB_sentiments = mnb.predict(X_test_stopWords)
 print(y_pred_stopWords_MNB_sentiments)
 print("Accuracy of the dataset using sentiments as a target using Multinomial Naive-Bayes is: ",
-      metrics.accuracy_score(Y_test, y_pred_stopWords_MNB_sentiments))
+      metrics.accuracy_score(Z_test, y_pred_stopWords_MNB_sentiments))
 performance.write("Confusion Matrix of sentiments using MNB with english stop words: \n")
 performance.write("Confusion Matrix of MNB sentiments:\n")
-print(confusion_matrix(Y_test, y_pred_stopWords_MNB_sentiments), file=performance)
+print(confusion_matrix(Z_test, y_pred_stopWords_MNB_sentiments), file=performance)
 performance.write("Classification report of MNB sentiments with english stop words: \n")
-print(classification_report(Y_test, y_pred_stopWords_MNB_sentiments), file=performance)
+print(classification_report(Z_test, y_pred_stopWords_MNB_sentiments), file=performance)
 print('----------------------------------------------------')
 
 # Top
